6a766e8ece567306f2296722f071353e82fc8c30
kshntn <kshntn@gmail.com>
2018-02-05 21:34:26 +0100

    new

--- app/src/main/java/com/example/android/earthquiz/MainActivity.java
+++ app/src/main/java/com/example/android/earthquiz/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.android.earthquiz;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -18,14 +17,15 @@ import android.widget.RadioGroup;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.text.NumberFormat;
-
 public class MainActivity extends AppCompatActivity {
+    static final String Score = "A";
+    static final String Continentnumber = "B";
+    static final String Submit_flag = "C";
+    static final String hintCounter = "D";
     ImageView mars;
     TextView firstHeader;
     int counter = 0;
-    int flag=0;
+    int flag = 0;
     Button hintButton;
     int numberOfContinents = 0;
     TextView numCont;
@@ -47,20 +47,14 @@ public class MainActivity extends AppCompatActivity {
     EditText answerTwo;
     EditText answerThree;
 
-
-    static final String Score = "A";
-    static final String Continentnumber="B";
-    static final String Submit_flag="C";
-    static final String hintCounter="D";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (savedInstanceState != null) {
             TotalScore = savedInstanceState.getInt(Score);
             numberOfContinents = savedInstanceState.getInt(Continentnumber);
-            flag=savedInstanceState.getInt(Submit_flag);
-            counter=savedInstanceState.getInt(hintCounter);
+            flag = savedInstanceState.getInt(Submit_flag);
+            counter = savedInstanceState.getInt(hintCounter);
         }
         setContentView(R.layout.activity_main);
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
@@ -73,8 +67,7 @@ public class MainActivity extends AppCompatActivity {
             hintButton.setText(getResources().getString(R.string.Hint1Question2));
         else
             hintButton.setText(getResources().getString(R.string.Hint0Question2));
-
-        if (flag==1){
+        if (flag == 1) {
             radio_Button1 = findViewById(R.id.answer1);
             radio_Button1.setBackgroundColor(getResources().getColor(R.color.CorrectAnswer));
             answerTwo = findViewById(R.id.answer2);
@@ -93,19 +86,18 @@ public class MainActivity extends AppCompatActivity {
             check4answer8 = findViewById(R.id.answer8Check4);
             check1answer8.setBackgroundColor(getResources().getColor(R.color.CorrectAnswer));
             check4answer8.setBackgroundColor(getResources().getColor(R.color.CorrectAnswer));
-            if(TotalScore<5){
-            firstHeader = findViewById(R.id.firstHeader);
-            firstHeader.setText(getResources().getString(R.string.FailIntro));
-            firstHeader.setBackgroundResource(R.color.firstHeaderColor);
-            mars = findViewById(R.id.intro);
-            mars.setImageResource(R.drawable.mars);
-            ScrollView test = findViewById(R.id.test);
-            test.setBackgroundResource(R.color.firstHeaderColor);
+            if (TotalScore < 5) {
+                firstHeader = findViewById(R.id.firstHeader);
+                firstHeader.setText(getResources().getString(R.string.FailIntro));
+                firstHeader.setBackgroundResource(R.color.firstHeaderColor);
+                mars = findViewById(R.id.intro);
+                mars.setImageResource(R.drawable.mars);
+                ScrollView test = findViewById(R.id.test);
+                test.setBackgroundResource(R.color.firstHeaderColor);
             }
             Button submit = findViewById(R.id.buttonSubmit);
             submit.setEnabled(false);
         }
-
     }
 
     public void onSaveInstanceState(Bundle savedInstanceState) {
@@ -116,14 +108,13 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
     }
 
-
     public void Hint(View view) {
         hintButton = findViewById(R.id.hint0);
         if (counter == 0) {
             Toast.makeText(getApplicationContext(), getResources().getString(R.string.Hint1), Toast.LENGTH_SHORT).show();
             hintButton.setText(getResources().getString(R.string.Hint1Question2));
         } else if (counter == 1) {
-            Toast.makeText(getApplicationContext(),getResources().getString(R.string.Hint2) , Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), getResources().getString(R.string.Hint2), Toast.LENGTH_SHORT).show();
             hintButton.setText(getResources().getString(R.string.Hint0Question2));
         } else {
             Toast.makeText(getApplicationContext(), getResources().getString(R.string.Hint3), Toast.LENGTH_SHORT).show();
@@ -147,14 +138,12 @@ public class MainActivity extends AppCompatActivity {
         if (radiobuttonID1 == firstradioID)
             TotalScore++;
 
-
         answerTwo = findViewById(R.id.answer2);
         String answerSt1 = answerTwo.getText().toString();
         if ((answerSt1.equals(getResources().getString(R.string.Answer1Question2))) || (answerSt1.equals(getResources().getString(R.string.Answer2Question2))) || (answerSt1.equals(getResources().getString(R.string.Answer3Question2))))
             TotalScore++;
         else if (answerSt1.equals(""))
             return -1;
-
 
         Radio_Group2 = findViewById(R.id.thirdRadio);
         int thirdradioID = Radio_Group2.getCheckedRadioButtonId();
@@ -216,29 +205,26 @@ public class MainActivity extends AppCompatActivity {
         check1answer8.setBackgroundColor(getResources().getColor(R.color.CorrectAnswer));
         check4answer8.setBackgroundColor(getResources().getColor(R.color.CorrectAnswer));
         return TotalScore;
-
-
     }
 
     public void submitScore(View view) {
-
         int x = calcScore();
         if (x == -1) {
             Toast.makeText(getApplicationContext(), getResources().getString(R.string.Submitwithpartialcomp), Toast.LENGTH_SHORT).show();
             return;
         }
         if (x == 8) {
-            Toast.makeText(getApplicationContext(), getName() + getResources().getString(R.string.Submitwithcomp,TotalScore), Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), getName() + getResources().getString(R.string.Submitwithcomp, TotalScore), Toast.LENGTH_LONG).show();
             Button submit = findViewById(R.id.buttonSubmit);
             submit.setEnabled(false);
-            flag=1;
+            flag = 1;
         } else if (x < 8 && x > 5) {
-            Toast.makeText(getApplicationContext(), getName() + getResources().getString(R.string.Submitwitherror,TotalScore), Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), getName() + getResources().getString(R.string.Submitwitherror, TotalScore), Toast.LENGTH_LONG).show();
             Button submit = findViewById(R.id.buttonSubmit);
             submit.setEnabled(false);
-            flag=1;
+            flag = 1;
         } else {
-            Toast.makeText(getApplicationContext(), getName() + getResources().getString(R.string.SubmitFail,TotalScore) , Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), getName() + getResources().getString(R.string.SubmitFail, TotalScore), Toast.LENGTH_LONG).show();
             firstHeader = findViewById(R.id.firstHeader);
             firstHeader.setText(getResources().getString(R.string.FailIntro));
             firstHeader.setBackgroundResource(R.color.firstHeaderColor);
@@ -248,12 +234,12 @@ public class MainActivity extends AppCompatActivity {
             test.setBackgroundResource(R.color.firstHeaderColor);
             Button submit = findViewById(R.id.buttonSubmit);
             submit.setEnabled(false);
-            flag=1;
+            flag = 1;
         }
     }
 
     public void Reset(View view) {
-        TotalScore=0;
+        TotalScore = 0;
         EditText nameField = findViewById(R.id.name);
         nameField.setText("");
         counter = 0;
@@ -296,7 +282,7 @@ public class MainActivity extends AppCompatActivity {
         check3answer8.setChecked(false);
         check4answer8.setChecked(false);
 
-        if (flag==1) {
+        if (flag == 1) {
             radio_Button1.setBackgroundColor(Color.TRANSPARENT);
             answerTwo.setTextColor(Color.BLACK);
             radio_Button2.setBackgroundColor(Color.TRANSPARENT);
@@ -306,7 +292,7 @@ public class MainActivity extends AppCompatActivity {
             radio_Button3.setBackgroundColor(Color.TRANSPARENT);
             check1answer8.setBackgroundColor(Color.TRANSPARENT);
             check4answer8.setBackgroundColor(Color.TRANSPARENT);
-            flag=0;
+            flag = 0;
         }
 
         firstHeader = findViewById(R.id.firstHeader);
@@ -318,14 +304,12 @@ public class MainActivity extends AppCompatActivity {
         test.setBackgroundResource(R.color.earthblue);
         Button submit = findViewById(R.id.buttonSubmit);
         submit.setEnabled(true);
-
     }
 
     public void increment(View view) {
         numberOfContinents = numberOfContinents + 1;
         numCont = findViewById(R.id.answer5);
         numCont.setText(String.valueOf(numberOfContinents));
-
     }
 
     public void decrement(View view) {
@@ -346,7 +330,7 @@ public class MainActivity extends AppCompatActivity {
             float x = ev.getRawX() + view.getLeft() - scrcoords[0];
             float y = ev.getRawY() + view.getTop() - scrcoords[1];
             if (x < view.getLeft() || x > view.getRight() || y < view.getTop() || y > view.getBottom())
-                ((InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow((this.getWindow().getDecorView().getApplicationWindowToken()), 0);
+                ((InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow((this.getWindow().getDecorView().getApplicationWindowToken()), 0);
         }
         return super.dispatchTouchEvent(ev);
     }
